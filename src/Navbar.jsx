877b56f987396656fad86c738fa73847a668772f
michaelrmartin <iammichaelmartin@gmail.com>
2023-07-12 12:33:51 -0500

    Adds axios and navbar elements

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,8 +1,26 @@
+// import React, { useState, useEffect } from "react";
+import "./Navbar.css";
+
 export function Navbar() {
+  //   const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.jwt);
+  //   const [userId, setUserId] = useState(localStorage.user_id);
+
+  //   useEffect(() => {
+  //     setIsLoggedIn(!!localStorage.jwt);
+  //     setUserId(localStorage.user_id);
+  //   }, []);
+
   return (
-    <nav>
-      <h1>HYKRZ</h1>
-      <a href="#">Home</a> | <a href="#">Link</a>
+    <nav className="navbar">
+      <h1 className="navbar--title">HYKRZ</h1>
+      <div className="navbar--nav">
+        <ul className="navbar--list">
+          <li className="navbar--list--item">
+            <a href="#">Home</a>
+          </li>
+          {/* <a href="/hikes">HYKZ</a> | <a href="/users/">HYKR</a> */}
+        </ul>
+      </div>
     </nav>
   );
 }
