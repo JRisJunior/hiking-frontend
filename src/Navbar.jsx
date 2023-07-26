941f12f195c84b41bd1dacd9538e5447a153e86a
michaelrmartin <iammichaelmartin@gmail.com>
2023-07-26 11:47:31 -0500

    Updates Navbar component links

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -12,17 +12,20 @@ export function Navbar() {
 
   return (
     <nav className="navbar">
-      <h1 className="navbar--title">HYKRZ</h1>
+      <h1 className="navbar--title">
+        <a href="#">HYK</a>
+      </h1>
       <div className="navbar--nav">
         <ul className="navbar--list">
           <li className="navbar--list--item">
-            <a href="#">Home</a>
+            <a href="#/hikes">HYKZ</a>
           </li>
-          <li className="navbar--list--item">
-            <a href="#">HYKZ</a>
-          </li>
+          <li className="navbar--list--item">{isLoggedIn ? <a href="/users">HYKRZ</a> : null}</li>
           <li className="navbar--list--item">
             {isLoggedIn ? <a href={`/users/${userId}`}>HYKR</a> : <a href="/signup">Signup</a>}
+          </li>
+          <li className="navbar--list--item">
+            {isLoggedIn ? <a href="/logout">Logout</a> : <a href="/login">Login</a>}
           </li>
         </ul>
       </div>
