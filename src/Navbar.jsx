7d483c474a09034412c77cc376d412530a20aabe
michaelrmartin <iammichaelmartin@gmail.com>
2023-08-02 11:11:01 -0500

    Adds Browser Router and routes

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import "./Navbar.css";
+import { LogoutLink } from "./LogoutLink";
 
 export function Navbar() {
   const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.jwt);
@@ -13,20 +14,18 @@ export function Navbar() {
   return (
     <nav className="navbar">
       <h1 className="navbar--title">
-        <a href="#">HYK</a>
+        <a href="/">HYK</a>
       </h1>
       <div className="navbar--nav">
         <ul className="navbar--list">
           <li className="navbar--list--item">
-            <a href="#/hikes">HYKZ</a>
+            <a href="/hikes">HYKZ</a>
           </li>
           <li className="navbar--list--item">{isLoggedIn ? <a href="/users">HYKRZ</a> : null}</li>
           <li className="navbar--list--item">
             {isLoggedIn ? <a href={`/users/${userId}`}>HYKR</a> : <a href="/signup">Signup</a>}
           </li>
-          <li className="navbar--list--item">
-            {isLoggedIn ? <a href="/logout">Logout</a> : <a href="/login">Login</a>}
-          </li>
+          <li className="navbar--list--item">{isLoggedIn ? <LogoutLink /> : <a href="/login">Login</a>}</li>
         </ul>
       </div>
     </nav>
